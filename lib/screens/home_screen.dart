c4048f2d2c045a56b9feba67b53a1ca91363c2d8
Iván Campos <108841072+is-Campos@users.noreply.github.com>
2024-11-03 01:29:42 -0600

    update: styles for the home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -7,15 +7,41 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Inicio')),
+      appBar: AppBar(
+        title: const Text(
+          'Inicio',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 24,
+          ),
+        ),
+        backgroundColor: Colors.blue,
+      ),
       body: Center(
         child: ElevatedButton(
           onPressed: () {
             context.go('/create-profile');
           },
-          child: const Text('Crear perfil'),
+          style: ElevatedButton.styleFrom(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15),
+            ),
+            backgroundColor: Colors.blue,
+            padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+            textStyle: const TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.w600,
+              color: Colors.white,
+            ),
+          ),
+          child: const Text(
+            'Crear perfil',
+            style: TextStyle(color: Colors.white),
+          ),
         ),
       ),
     );
   }
 }
+
